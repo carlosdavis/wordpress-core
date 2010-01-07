1686700236e00f73c6d09107318bd1924f590053
Carlos Drew d'Avis <carlosdavis@gmail.com>
2010-01-07 08:57:33 -0600

    Hmmm?

--- wp-content/plugins/random-posts-plugin/uninstall.php
+++
@@ -1,7 +0,0 @@
-<?php
-	if (defined('ABSPATH') && defined('WP_UNINSTALL_PLUGIN') 
-	&& strtolower(WP_UNINSTALL_PLUGIN) === 'random-posts-plugin/random-posts.php') {
-		delete_option('random-posts');
-		delete_option('widget_rrm_random_posts');
-	}
-?>
--- wp-content/plugins/recent-comments-plugin/uninstall.php
+++
@@ -1,7 +0,0 @@
-<?php
-	if (defined('ABSPATH') && defined('WP_UNINSTALL_PLUGIN') 
-	&& strtolower(WP_UNINSTALL_PLUGIN) === 'recent-comments-plugin/recent-comments.php') {
-		delete_option('recent-comments');
-		delete_option('widget_rrm_recent_comments');
-	}
-?>
